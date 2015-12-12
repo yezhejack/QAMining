cfc44198913c4b05f76c1ebfb612dd67b409a2e8
Jack's Mac <yezhejack@gmail.com>
2015-12-12 20:46:20 +0800

    modify tagdata.py to continue the work

--- tagdata.py
+++ tagdata.py
@@ -16,6 +16,7 @@ if __name__=='__main__':
     parser.add_argument("-i","--input",help="the file name of subtitile. The fils should be in data/. default=subtitle.dat",default="subtitle.dat")
     parser.add_argument("-o","--output",help="The file name of output result.default=QA_subtitle.txt",default="QA_subtitle.txt")
     parser.add_argument("-n","--new",help='Create a new file',action='store_true')
+    parser.add_argument("-p","--position",help='the file contains the last index,default=tagdatapos.dat',default='tagdatapos.dat')
     args=parser.parse_args()
 
     last_index=0
@@ -24,14 +25,10 @@ if __name__=='__main__':
         output_file.close()
     else:
         #recover from the output
-        output_file=open(current_path+'/data/'+args.output,'r')
-        sentence=output_file.readline()
-        while sentence!='':
-            last_index=int(sentence)
-            sentence=output_file.readline()
-            sentence=output_file.readline()
-            sentence=output_file.readline()
-        output_file.close()
+        f=open(current_path+'/data/'+args.position,'r')
+        sentence=f.readline()
+        last_index=int(sentence)
+        f.close()
 
     input_file=open(current_path+'/data/'+args.input,'r')
     output_file=open(current_path+'/data/'+args.output,'a')
@@ -47,9 +44,20 @@ if __name__=='__main__':
         print 'No.'+str(i)
         print sen_db[i].strip()
         print sen_db[i+1].strip()
-        ans=raw_input('input y or n:')
+        ans=raw_input('input y or n or exit:')
         if ans=='y':
             output_file.write(str(i)+'\n'+'Q:'+sen_db[i].strip()+'\n'+'A:'+sen_db[i+1].strip()+'\n')
+        if ans=='exit':
+            f=open(current_path+'/data/'+args.position,'w')
+            f.write(str(i-1))
+            f.close()
+            input_file.close()
+            output_file.close()
+            exit()
+            
+        f=open(current_path+'/data/'+args.position,'w')
+        f.write(str(i))
+        f.close()
 
     input_file.close()
     output_file.close()
