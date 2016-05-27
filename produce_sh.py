eba5ea31b25c4c37e85c1278825f30983ffe7651
Jack's Mac <yezhejack@gmail.com>
2016-05-28 00:11:05 +0800

    work update

--- produce_sh.py
+++ produce_sh.py
@@ -1,9 +1,16 @@
 #coding:utf-8
 if __name__=="__main__":
-    template="{\n    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_%s.dat --method PrefixSpan --ispercent --minsup 0.%s\n    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_%s.dat --test_input tagged_subtitle_2.dat --method 1\n}&"
-    f=open("work2.sh","w")
-    for i in range(1,6):
-        per=str(1036-2*i)
-        cmd=template % (per,per,per)
-        f.write(cmd+"\n")
+    num_groups=12
+    num_tasks_of_group=4
+    template="\n    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_%s.dat --method PrefixSpan --ispercent --minsup 0.%s\n    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_%s.dat --test_input tagged_subtitle_2.dat --method 1\n"
+    param=900
+    f=open("work.sh","w")
+    for i in range(0,num_groups):
+        f.write("{")
+        for j in range(0,num_tasks_of_group):
+            per="0"+str(param)
+            param+=2
+            cmd=template % (per,per,per)
+            f.write(cmd)
+        f.write("}&\n")
     f.close()
--- work.sh
+++ work.sh
@@ -1,8 +1,156 @@
 {
-    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --method PrefixSpan --ispercent --minsup 0.1044 >/dev/null 2>&1
-    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --test_input tagged_subtitle_2.dat --method 1 >>1044.txt 2>&1
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0900.dat --method PrefixSpan --ispercent --minsup 0.0900
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0900.dat --test_input tagged_subtitle_2.dat --method 1
+
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0902.dat --method PrefixSpan --ispercent --minsup 0.0902
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0902.dat --test_input tagged_subtitle_2.dat --method 1
+
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0904.dat --method PrefixSpan --ispercent --minsup 0.0904
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0904.dat --test_input tagged_subtitle_2.dat --method 1
+
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0906.dat --method PrefixSpan --ispercent --minsup 0.0906
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0906.dat --test_input tagged_subtitle_2.dat --method 1
 }&
 {
-    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --method PrefixSpan --ispercent --minsup 0.1044 >/dev/null 2>&1
-    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --test_input tagged_subtitle_2.dat --method 1 >>1044.txt 2>&1
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0908.dat --method PrefixSpan --ispercent --minsup 0.0908
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0908.dat --test_input tagged_subtitle_2.dat --method 1
+
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0910.dat --method PrefixSpan --ispercent --minsup 0.0910
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0910.dat --test_input tagged_subtitle_2.dat --method 1
+
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0912.dat --method PrefixSpan --ispercent --minsup 0.0912
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0912.dat --test_input tagged_subtitle_2.dat --method 1
+
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0914.dat --method PrefixSpan --ispercent --minsup 0.0914
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0914.dat --test_input tagged_subtitle_2.dat --method 1
+}&
+{
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0916.dat --method PrefixSpan --ispercent --minsup 0.0916
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0916.dat --test_input tagged_subtitle_2.dat --method 1
+
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0918.dat --method PrefixSpan --ispercent --minsup 0.0918
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0918.dat --test_input tagged_subtitle_2.dat --method 1
+
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0920.dat --method PrefixSpan --ispercent --minsup 0.0920
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0920.dat --test_input tagged_subtitle_2.dat --method 1
+
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0922.dat --method PrefixSpan --ispercent --minsup 0.0922
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0922.dat --test_input tagged_subtitle_2.dat --method 1
+}&
+{
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0924.dat --method PrefixSpan --ispercent --minsup 0.0924
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0924.dat --test_input tagged_subtitle_2.dat --method 1
+
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0926.dat --method PrefixSpan --ispercent --minsup 0.0926
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0926.dat --test_input tagged_subtitle_2.dat --method 1
+
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0928.dat --method PrefixSpan --ispercent --minsup 0.0928
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0928.dat --test_input tagged_subtitle_2.dat --method 1
+
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0930.dat --method PrefixSpan --ispercent --minsup 0.0930
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0930.dat --test_input tagged_subtitle_2.dat --method 1
+}&
+{
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0932.dat --method PrefixSpan --ispercent --minsup 0.0932
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0932.dat --test_input tagged_subtitle_2.dat --method 1
+
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0934.dat --method PrefixSpan --ispercent --minsup 0.0934
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0934.dat --test_input tagged_subtitle_2.dat --method 1
+
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0936.dat --method PrefixSpan --ispercent --minsup 0.0936
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0936.dat --test_input tagged_subtitle_2.dat --method 1
+
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0938.dat --method PrefixSpan --ispercent --minsup 0.0938
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0938.dat --test_input tagged_subtitle_2.dat --method 1
+}&
+{
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0940.dat --method PrefixSpan --ispercent --minsup 0.0940
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0940.dat --test_input tagged_subtitle_2.dat --method 1
+
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0942.dat --method PrefixSpan --ispercent --minsup 0.0942
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0942.dat --test_input tagged_subtitle_2.dat --method 1
+
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0944.dat --method PrefixSpan --ispercent --minsup 0.0944
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0944.dat --test_input tagged_subtitle_2.dat --method 1
+
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0946.dat --method PrefixSpan --ispercent --minsup 0.0946
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0946.dat --test_input tagged_subtitle_2.dat --method 1
+}&
+{
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0948.dat --method PrefixSpan --ispercent --minsup 0.0948
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0948.dat --test_input tagged_subtitle_2.dat --method 1
+
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0950.dat --method PrefixSpan --ispercent --minsup 0.0950
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0950.dat --test_input tagged_subtitle_2.dat --method 1
+
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0952.dat --method PrefixSpan --ispercent --minsup 0.0952
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0952.dat --test_input tagged_subtitle_2.dat --method 1
+
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0954.dat --method PrefixSpan --ispercent --minsup 0.0954
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0954.dat --test_input tagged_subtitle_2.dat --method 1
+}&
+{
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0956.dat --method PrefixSpan --ispercent --minsup 0.0956
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0956.dat --test_input tagged_subtitle_2.dat --method 1
+
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0958.dat --method PrefixSpan --ispercent --minsup 0.0958
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0958.dat --test_input tagged_subtitle_2.dat --method 1
+
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0960.dat --method PrefixSpan --ispercent --minsup 0.0960
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0960.dat --test_input tagged_subtitle_2.dat --method 1
+
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0962.dat --method PrefixSpan --ispercent --minsup 0.0962
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0962.dat --test_input tagged_subtitle_2.dat --method 1
+}&
+{
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0964.dat --method PrefixSpan --ispercent --minsup 0.0964
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0964.dat --test_input tagged_subtitle_2.dat --method 1
+
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0966.dat --method PrefixSpan --ispercent --minsup 0.0966
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0966.dat --test_input tagged_subtitle_2.dat --method 1
+
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0968.dat --method PrefixSpan --ispercent --minsup 0.0968
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0968.dat --test_input tagged_subtitle_2.dat --method 1
+
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0970.dat --method PrefixSpan --ispercent --minsup 0.0970
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0970.dat --test_input tagged_subtitle_2.dat --method 1
+}&
+{
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0972.dat --method PrefixSpan --ispercent --minsup 0.0972
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0972.dat --test_input tagged_subtitle_2.dat --method 1
+
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0974.dat --method PrefixSpan --ispercent --minsup 0.0974
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0974.dat --test_input tagged_subtitle_2.dat --method 1
+
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0976.dat --method PrefixSpan --ispercent --minsup 0.0976
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0976.dat --test_input tagged_subtitle_2.dat --method 1
+
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0978.dat --method PrefixSpan --ispercent --minsup 0.0978
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0978.dat --test_input tagged_subtitle_2.dat --method 1
+}&
+{
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0980.dat --method PrefixSpan --ispercent --minsup 0.0980
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0980.dat --test_input tagged_subtitle_2.dat --method 1
+
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0982.dat --method PrefixSpan --ispercent --minsup 0.0982
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0982.dat --test_input tagged_subtitle_2.dat --method 1
+
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0984.dat --method PrefixSpan --ispercent --minsup 0.0984
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0984.dat --test_input tagged_subtitle_2.dat --method 1
+
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0986.dat --method PrefixSpan --ispercent --minsup 0.0986
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0986.dat --test_input tagged_subtitle_2.dat --method 1
+}&
+{
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0988.dat --method PrefixSpan --ispercent --minsup 0.0988
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0988.dat --test_input tagged_subtitle_2.dat --method 1
+
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0990.dat --method PrefixSpan --ispercent --minsup 0.0990
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0990.dat --test_input tagged_subtitle_2.dat --method 1
+
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0992.dat --method PrefixSpan --ispercent --minsup 0.0992
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0992.dat --test_input tagged_subtitle_2.dat --method 1
+
+    python pattern_mine.py --input tagged_dialogues_positive_simple_5.dat --output patterns_0994.dat --method PrefixSpan --ispercent --minsup 0.0994
+    python classifier.py --pos_input tagged_dialogues_positive_simple_5.dat --neg_input tagged_dialogues_negative_simple_5.dat --pat_input patterns_0994.dat --test_input tagged_subtitle_2.dat --method 1
 }&
